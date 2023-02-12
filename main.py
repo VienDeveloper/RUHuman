2bdfa9375059de13a2b43afecd6d401490a397b2
Vien Catigan <viendev.py@gmail.com>
2023-02-12 04:07:27 +0000

    YES

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+import os # default module
+from dotenv import load_dotenv
+
+load_dotenv() # load all the variables from the env file
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+
+bot.run(os.getenv('TOKEN')) # run the bot with the token
